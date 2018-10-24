187475d591f127c6ee9de7afc87f3b3689ceadfc
DuyHo1 <hoduy040397@gmail.com>
2018-10-24 23:05:27 +0700

    Buoc 5

--- global (heroku).js
+++ global (heroku).js
@@ -8,11 +8,11 @@ module.exports = {
         database: 'qldd'
     },
     db_postgres: {
-        host: 'ec2-23-23-216-40.compute-1.amazonaws.com',
-        user: 'cjgmzlabuhqtfi',
-        password: 'd622e3da2a297c8ace6627c86463b44033c60f0c501ab16a545521d0b23cacf3',
+        host: 'ec2-174-129-18-98.compute-1.amazonaws.com',
+        user: 'zyrwqvrioijwyi',
+        password: '35700aa27077780e8aad846bb17c1ba571b503608c8aa99a66311f35ab8c29f1',
         port:'5432',
-        database: 'depna5rf8ertn0'
+        database: 'den0ffe4o9gn9b'
     },
     //db_postgres: {
     //    host: 'localhost',
@@ -21,7 +21,7 @@ module.exports = {
     //    port:'5432',
     //    database: 'qldd'
     //},
-    host: 'https://tpltesting.herokuapp.com',
+    host: 'https://myparkhangseo.herokuapp.com',
     email_setting: {
         host: 'smtp.office365.com', // Office 365 server
         port: 587, // secure SMTP
